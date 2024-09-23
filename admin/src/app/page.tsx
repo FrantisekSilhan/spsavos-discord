9b9ea81ace85a0ad8ee99375f795624db30e8658
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-09-23 11:44:52 +0200

    feat(auth): discord login

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,6 +1,20 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
+  const {data: session, status } = useSession();
+
+  if (!session) {
+    return (
+      <div>
+        <p>You are not signed in</p>
+        <button onClick={() => signIn("discord")}>Sign in with Discord</button>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
