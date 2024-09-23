ca7cee2c9c1f418ab2738b1104112dad8c375f7e
MartinRostejnsky <martin.rostejnsky@email.cz>
2024-09-23 12:18:12 +0200

    feat(admin/auth): get user info from discord API

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
   const {data: session, status } = useSession({required: true});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://discord.com//api/v10/users/@me", {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if (session) {
+      fetchData();
+    }
+  }, [session]);
 
   if (!session) {
     return (
@@ -18,6 +39,7 @@ export default function Home() {
     <div>
       <p>Signed in as {session.accessToken}</p>
       <button onClick={() => signOut()}>Sign out</button>
+
 
     </div>
   )
